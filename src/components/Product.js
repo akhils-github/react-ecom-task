ad7499110130acc58f1784cfe2f33f39b10d6820
akhil <akhilsidk@gmail.com>
2024-03-31 20:04:13 +0530

    updated

--- src/components/Product.js
+++ src/components/Product.js
@@ -5,12 +5,13 @@ import { BsPlus, BsEyeFill } from "react-icons/bs";
 
 import { CartContext } from "../contexts/CartContext";
 
-const Product = ({ product }) => {
+const Products = ({ product }) => {
   const { addToCart } = useContext(CartContext);
 
   // destructure product
   const { id, images, category, title, price } = product;
   console.log(product);
+  // return <Slideshow images={images} />;
   return (
     <div>
       <div className="border border-[#e4e4e4] h-[300px] mb-4 relative overflow-hidden group transition">
@@ -41,7 +42,9 @@ const Product = ({ product }) => {
       </div>
       {/* category, title & price */}
       <div>
-        <div className="tex-sm capitalize text-gray-500 mb-1">{category?.name}</div>
+        <div className="tex-sm capitalize text-gray-500 mb-1">
+          {category?.name}
+        </div>
         <Link to={`/product/${id}`}>
           <h2 className="font-semibold mb-1">{title}</h2>
         </Link>
@@ -52,4 +55,92 @@ const Product = ({ product }) => {
   );
 };
 
+// export default Product;
+
+const colors = ["#0088FE", "#00C49F", "#FFBB28"];
+const delay = 2500;
+
+function Product({ product }) {
+  const { addToCart } = useContext(CartContext);
+
+  // destructure product
+  const { id, images, category, title, price } = product;
+  console.log(images, colors);
+  const [index, setIndex] = React.useState(0);
+  const timeoutRef = React.useRef(null);
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === images?.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
+
+  return (
+    <>
+      <div className="slideshow">
+        <div className="border border-[#e4e4e4] relative overflow-hidden group transition">
+          <div
+            className="slideshowSlider relative"
+            style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+          >
+            {images.map((backgroundColor, index) => (
+              <div className="slide rounded" key={index}>
+                <img src={backgroundColor} alt="" className="rounded" />
+              </div>
+            ))}
+          </div>
+          <div className="absolute top-6 -right-11 group-hover:right-5 p-2 flex flex-col justify-center items-center gap-y-2 opacity-0 group-hover:opacity-100 transition-all duration-300">
+            <button onClick={() => addToCart(product, id)}>
+              <div className="flex justify-center items-center text-white w-12 h-12 bg-teal-500">
+                <BsPlus className="text-3xl" />
+              </div>
+            </button>
+            <Link
+              to={`/product/${id}`}
+              className="w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl"
+            >
+              <BsEyeFill />
+            </Link>
+          </div>
+        </div>
+
+        <div className="relative my-2">
+          <div className="tex-sm capitalize text-gray-500 mb-1 text-center">
+            {category?.name}
+          </div>
+          <Link to={`/product/${id}`}>
+            <h2 className="font-semibold mb-1">{title}</h2>
+          </Link>
+
+          <h2 className="font-semibbold">$ {price}</h2>
+        </div>
+      </div>
+      {/* <div>
+        <div className="tex-sm capitalize text-gray-500 mb-1">
+          {category?.name}
+        </div>
+        <Link to={`/product/${id}`}>
+          <h2 className="font-semibold mb-1">{title}</h2>
+        </Link>
+
+        <h2 className="font-semibbold">$ {price}</h2>
+      </div> */}
+    </>
+  );
+}
 export default Product;
--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -20,6 +20,32 @@ const ProductDetails = () => {
     enabled: !!id,
   });
   console.log(productDetails);
+  // const { title, price, description, images } = productDetails;
+
+  const [index, setIndex] = React.useState(0);
+  const timeoutRef = React.useRef(null);
+  const delay = 2500;
+
+  function resetTimeout() {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+    }
+  }
+
+  React.useEffect(() => {
+    resetTimeout();
+    timeoutRef.current = setTimeout(
+      () =>
+        setIndex((prevIndex) =>
+          prevIndex === productDetails?.images?.length - 1 ? 0 : prevIndex + 1
+        ),
+      delay
+    );
+
+    return () => {
+      resetTimeout();
+    };
+  }, [index]);
 
   //get the single product based on id
 
@@ -29,8 +55,8 @@ const ProductDetails = () => {
   }
 
   // destructure product
-  const { title, price, description, images } = productDetails;
-  console.log(title, images);
+  // console.log(title, images);
+
   return (
     <section className="pt-[450px] md:pt-32 pb-[400px] md:pb-12 lg:py-32 h-screen flex items-center">
       <div className="container mx-auto">
@@ -38,17 +64,33 @@ const ProductDetails = () => {
         <div className="flex flex-col lg:flex-row items-center">
           {/* image */}
           <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
-            <img className="max-w-[200px] lg:max-w-xs" src={images[0]} alt="" />
+            <div className="slideshow">
+              <div className="border border-[#e4e4e4] relative overflow-hidden group transition">
+                <div
+                  className="slideshowSlider relative"
+                  style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+                >
+                  {productDetails?.images?.map((backgroundColor, index) => (
+                    <div className="slide rounded" key={index}>
+                      <img src={backgroundColor} alt="" className="rounded" />
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
           </div>
+          {/* <div className="flex flex-1 justify-center items-center mb-8 lg:mb-0">
+            <img className="max-w-[200px] lg:max-w-xs" src={images[0]} alt="" />
+          </div> */}
           {/* text */}
           <div className="flex-1 text-center lg:text-left">
             <h1 className="text-[26px] font-medium mb-2 max-w-[450px] mx-auto lg:mx-0">
-              {title}
+              {productDetails?.title}
             </h1>
             <div className="text-2xl text-red-500 font-medium mb-6">
-              $ {price}
+              $ {productDetails?.price}
             </div>
-            <p className="mb-8">{description}</p>
+            <p className="mb-8">{productDetails?.description}</p>
             <button
               onClick={() => addToCart(productDetails, productDetails?.id)}
               className="bg-primary py-4 px-8 text-white"
